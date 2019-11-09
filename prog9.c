6c9689136eb321ffbd9abe7cf76d2a5cdcc6b3e8
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-09 14:52:18 +0530

    prog9.c

---
+++ prog9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,len;
+    char a[100];
+    gets(a);
+    len=strlen(a);
+    for(i=0;i!=len;i++)
+    {
+        printf("address of character %c = %d \n",a[i],a[i]);
+    }
+    puts(a);
+}
