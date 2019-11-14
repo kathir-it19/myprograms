8301d7f08fa5be796e50677345d2bc398868ef8c
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-14 11:38:19 +0530

    vote.py

---
+++ vote.py
@@ -0,0 +1,9 @@
+ name=(input("enter the name"))
+age=int(input("enter your age"))
+afage=age+100;
+print('after 100 year later your age',afage)
+if(age>18):
+ print("you are eligble for vote")
+else:
+    print("you are noteligble for vote")
+
