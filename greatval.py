32eb197acfba391a8a9ef5b4422d74550feec1a1
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-14 11:47:13 +0530

    greatval.py

--- greatval.py
+++ greatval.py
@@ -1,7 +1,14 @@
+def find_max(a,b):
+   if(a>b):
+        return a;
+   else:
+       return b;
 
-x=int(input("enter the value X"))
-y=int(input("enter the value Y"))
-if(x>y):
- print(x)
-else:
- print(y)
+
+a = int(input("enter first value"))
+b = int(input("enter second value"))
+max = find_max(a,b)
+print("the max value",max)
+
+
+
