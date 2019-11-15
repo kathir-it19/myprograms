381ba8bdfde7736e1253920ad349381d683f2b52
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-15 20:52:51 +0530

    prog55.c

---
+++ prog55.c
@@ -0,0 +1,15 @@
+ a=int(input("enter your mark"))
+if(a>95):
+   print("o greade")
+elif(a>90and a<=95):
+   print("A grade") 
+elif(a>80and a<=90):
+   print("B grade") 
+elif(a>70and a<=80):
+   print("C grade") 
+elif(a>60and a<=70):
+   print("D grade") 
+elif(a>50and a<=60):
+   print("E grade") 
+else:
+   print("F grade") 
