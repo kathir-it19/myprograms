426cb326e604e506d5601a1d796cf76a9feffa29
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-21 14:15:55 +0530

    studentmark.py

---
+++ studentmark.py
@@ -0,0 +1,13 @@
+name=input("enter your name:")
+roll=input("enter your roll no:")
+m1=float(input("enter maths mark:"))
+m2=float(input("enter pysics mark:"))
+m3=float(input("enter chemistry mark:"))
+m4=float(input("enter pythin  mark:"))
+total=m1+m2+m3+m4
+avg=total/4
+print ("***************STUDENT MARK SHEET************")
+print ("Name:",name)
+print("Roll no:",roll)
+print("TOTAL MARK:",total)
+print("AVERAGE:",avg)
