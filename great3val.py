d67bc0aa4bdf33fcd05b3e1e62e2465151c3f048
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-22 09:25:35 +0530

    great3value.py

---
+++ great3val.py
@@ -0,0 +1,10 @@
+  a=int(input("enter the first value:"))
+b=int(input("enter the second value:"))
+c=int(input("enter the thired value:"))
+if(a>b)and(a>c):
+  print ('the greatest value is A=',a)
+elif(b>c):
+  print("the greatest value is B=",b)
+else:
+  print("the greatest value is C=",c)
+
