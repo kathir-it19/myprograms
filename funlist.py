f1058765e08b6facd746410253098a68692e5f8e
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-26 11:18:11 +0530

    funlist.py

---
+++ funlist.py
@@ -0,0 +1,8 @@
+def fun(m):
+    print("in fun before change:",m)
+    m=[1,2,3,4,5,6,7] #m[1]="hi"
+    print("in fun after change:",m)
+k=["k","a","t","h","i","r"]
+print("before change:",k)
+fun(k)
+print("after change:",k)
