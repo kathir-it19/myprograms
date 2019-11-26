212ee36e333318fca4b6f5670ae59c7a67437436
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-26 10:06:10 +0530

    del,rem,pop,extend,add.py

---
+++ del,rem,pop,extend,add.py
@@ -0,0 +1,15 @@
+a=[23,30]
+a.extend((1,3,"kathir"))
+print(a)
+a.pop()
+a.pop(2)
+print(a)
+a.clear()
+print(a)
+a.remove()
+print(a)
+sum=0
+a=[1,2,3,4,5,6]
+for i in a:
+    sum=sum+i
+print(sum)
