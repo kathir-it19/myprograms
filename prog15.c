937c080f820ab539d85a1261c24251cabdcd77e9
kathir-it19 <53468735+kathir-it19@users.noreply.github.com>
2019-11-28 11:52:16 +0530

    prog15.c

---
+++ prog15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ 
+    int i,a[100];
+  printf("enter the value:");
+  for(i=0;i<=10;i++)
+  {
+  scanf("%d",&a[i]);
+  }
+for(i=01;i<=10;i++)
+{
+  if(a[i]==23)
+  {
+       printf("It is equal to 23=%d\n",a[i]);   
+  }
+}
+}
